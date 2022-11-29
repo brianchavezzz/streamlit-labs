a9a16b43f6f38f3316c4f9056577c0dca54bf66e
brianchavezzz <117377597+brianchavezzz@users.noreply.github.com>
2022-11-28 23:14:57 -0600

    Update TAREAS_LUNES.py

--- TAREAS_LUNES.py
+++ TAREAS_LUNES.py
@@ -58,35 +58,34 @@ if opcion=='TAREA_4':
     st.subheader('Tema 4')
     st.text('Viajes de Uber en la ciudad de Nueva York con filtros por hora. ')
 
+    @st.cache 
 
-   
-    data = pd.read_csv("probartarea3.csv", nrows=10000) 
+    def load_data(nrows): 
+            data = pd.read_csv("probartarea3.csv", nrows=nrows) 
 
-    lowercase = lambda x: str(x).lower() 
+            lowercase = lambda x: str(x).lower() 
 
-    data.rename(lowercase, axis='columns', inplace=True) 
+            data.rename(lowercase, axis='columns', inplace=True) 
 
-    data=data.dropna()
+            data=data.dropna()
 
-    data['date/time'] = pd.to_datetime(data['date/time']) 
+            data['date/time'] = pd.to_datetime(data['date/time']) 
 
-    data["hora"]=[(hora.hour) for hora in data["date/time"]]  # probar con 100, 1000, etc
+            data["hora"]=[(hora.hour) for hora in data["date/time"]]
+
+            return data 
+
+    data = load_data(10000) # probar con 100, 1000, etc
 
 
     dts = st.slider('Rango de fechas: ',
-            0,23,4
-            )
+                0,23,4
+                )
 
     data=data[(data['hora'] == dts)]
 
 
 
-    mapa = px.scatter_mapbox(data, lat="lat", lon="lon",size_max=13, zoom=10,width=900, height=450)
-    mapa.update_coloraxes(colorbar_tickfont_size=10)
-    mapa.update_layout(mapbox_style="open-street-map")
-    mapa.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-        
-    st.plotly_chart(mapa)
-
+    st.map(data,10,True)
 
 
